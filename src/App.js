fcdc48826a07845feb628aea20eacb5af95023f2
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2023-06-27 14:49:03 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Counter from './Counter';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Counter />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/Counter.jsx
@@ -0,0 +1,5 @@
+import CountUp from "react-countup";
+
+export default function Counter() {
+  return <CountUp start={0} end={1000} />;
+}
